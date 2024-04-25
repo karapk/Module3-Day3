d264657d8316765b80af345ac415e68dcf0ec63e
Philip Karanja <kpk.my.mailings@gmail.com>
2024-04-24 19:00:14 -0500

    json learning and troubleshooting

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import fs from 'fs';
 import _ from 'lodash'
-import { color, log, red, green } from 'console-log-colors'
+import { color, log, red, green } from 'console-log-colors';
 
 const myarray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -11,4 +12,11 @@ console.log(output);
 
 
 
+//Sync File Access
+const file = fs.readFileSync('./data.json', {encoding: 'utf8'});
+console.log(file);
+
+// An Async File Access
+
+fs.readFile('./data.json', {encoding: 'utf8'}, (err, f) => console.log(color.green(f)));
 
